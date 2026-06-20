d9e8b0c1407dd92c24c1fb0f1a88b6ff7a0080d7
SarahPark-DS <sarah4282@gmail.com>
2026-06-20 17:14:41 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ trail0/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = -1;
+
+    while(n != 0){
+        cin >> n;
+        if(n == 0){
+            break;
+        }
+        cout << n << endl;
+
+    }
+    return 0;
+}
