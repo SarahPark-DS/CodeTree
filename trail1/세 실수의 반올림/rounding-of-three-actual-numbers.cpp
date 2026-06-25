11c8bf7261baeef109ae40e23d54f7a1425a337a
SarahPark-DS <sarah4282@gmail.com>
2026-06-25 12:11:06 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail1/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    return 0;
+}
