96479dada9204b3d89c60c32accdaf11e089963e
SarahPark-DS <sarah4282@gmail.com>
2026-06-25 12:56:17 +0900

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, x, y;
+    char c;
+
+    cin >> a >> c >> x >> c >> y;
+
+    cout << "010" << "-" << y << "-" << x;
+
+    return 0;
+}
