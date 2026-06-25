843ea8a6f5eb0bfd5f88d734330fea7d29eaf2ce
SarahPark-DS <sarah4282@gmail.com>
2026-06-25 12:54:18 +0900

    [Passed] 날짜 변경하여 출력 (2ms, 0MB)

---
+++ trail1/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int month, day, year;
+    char c;
+
+    cin >> year >> c >> month >> c >> day;
+
+    cout << month << "-" << day << "-" << year;
+    return 0;
+}
