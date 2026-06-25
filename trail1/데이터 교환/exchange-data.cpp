b9637cd09201f336f0066f3b5021ac67ab5d6a2b
SarahPark-DS <sarah4282@gmail.com>
2026-06-25 11:35:05 +0900

    [Passed] 데이터 교환 (2ms, 0MB)

---
+++ trail1/데이터 교환/exchange-data.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5, b = 6, c = 7;
+    int temp1, temp2;
+
+    temp1 = b;
+    temp2 = c;
+
+    b = a;
+    c = temp1;
+    a = temp2;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}
